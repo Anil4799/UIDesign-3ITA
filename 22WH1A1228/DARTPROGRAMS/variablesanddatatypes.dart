524ddb6d5860646c78a861c6685c05017b07adfc
Sritha <121919073+Sritha22wh1a1228@users.noreply.github.com>
2024-09-08 20:25:57 +0530

    Update variablesanddatatypes.dart

--- 22WH1A1228/DARTPROGRAMS/variablesanddatatypes.dart
+++ 22WH1A1228/DARTPROGRAMS/variablesanddatatypes.dart
@@ -1,1 +1,10 @@
-
+void main(){
+  String name="John";
+  int roll=25;
+  double pie=3.14;
+  bool present=false;
+  print('Name: $name');
+  print('Age: $roll');
+  print('Height: $pie');
+  print('Is Student: $present');
+}
